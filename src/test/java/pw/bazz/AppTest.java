098fcdd7738f0b51278ed1c23f773f7a4fbe3da0
Oleg <obazylnikov@ggc.edu>
2021-03-21 13:49:59 -0400

    initial commit

---
+++ src/test/java/pw/bazz/AppTest.java
@@ -0,0 +1,20 @@
+package pw.bazz;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
